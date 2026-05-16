2592c260e2f48c5268f130bf962b6652e6478914
alrafat2002 <alrafat71@gmail.com>
2026-05-17 02:24:11 +0600

    form added to vendorpayment

--- views/vendor_payment.php
+++ views/vendor_payment.php
@@ -36,6 +36,109 @@ $shipping_address = trim(
 
 <h2>Select Payment Method</h2>
 
+<form
+method="POST"
+action="../controllers/VendorCartController.php?action=payment"
+id="payment-form"
+>
+
+<input
+type="hidden"
+name="total_amount"
+value="<?= $total_amount ?>"
+>
+
+<input
+type="hidden"
+name="shipping_address"
+value="<?= htmlspecialchars($shipping_address) ?>"
+>
+
+<label>
+
+<input
+type="radio"
+name="payment_method"
+value="Credit Card"
+required
+>
+
+Credit Card
+
+</label>
+
+<br><br>
+
+<label>
+
+<input
+type="radio"
+name="payment_method"
+value="bKash"
+required
+>
+
+bKash
+
+</label>
+
+<br><br>
+
+<label>
+
+<input
+type="radio"
+name="payment_method"
+value="Nagad"
+required
+>
+
+Nagad
+
+</label>
+
+<br><br>
+
+<label>
+
+<input
+type="radio"
+name="payment_method"
+value="Bank Transfer"
+required
+>
+
+Bank Transfer
+
+</label>
+
+<br><br>
+
+<label>
+
+<input
+type="radio"
+name="payment_method"
+value="Cash on Delivery"
+required
+>
+
+Cash on Delivery
+
+</label>
+
+<br><br>
+
+<h3>
+Total Payable: ৳<?= $total_amount ?>
+</h3>
+
+<button type="submit">
+Confirm Order
+</button>
+
+</form>
+
 
 
 </body>
