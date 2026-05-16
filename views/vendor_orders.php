9b863ea7142ad2d6b6a9e37e96346a360bec3a34
alrafat2002 <alrafat71@gmail.com>
2026-05-17 02:21:53 +0600

    table added to vendororders

--- views/vendor_orders.php
+++ views/vendor_orders.php
@@ -15,5 +15,36 @@ $orders = OrderModel::getOrders($user_id);
     <title>Vendor Orders</title>
 </head>
 
+<body>
 
+<h2>Successful Orders</h2>
+
+<table border="1" cellpadding="10">
+
+<tr>
+    <th>Order ID</th>
+    <th>Total</th>
+    <th>Status</th>
+    <th>Date</th>
+</tr>
+
+<?php while($order = $orders->fetch_assoc()): ?>
+
+<tr>
+
+<td><?= $order['id'] ?></td>
+
+<td><?= $order['total_amount'] ?></td>
+
+<td><?= $order['status'] ?></td>
+
+<td><?= $order['order_date'] ?></td>
+
+</tr>
+
+<?php endwhile; ?>
+
+</table>
+
+</body>
 </html>
